9af8d90b14fb06cfa583f721be0e249ae415e753
c-o-d-e <swapyuk@gmail.com>
2022-03-17 09:29:47 +0000

    Final touches to the boilerplate

--- script.js
+++ script.js
@@ -8,7 +8,7 @@ canvas.height = window.innerHeight;
 const mouse = {
   x: null,
   y: null,
-  radius: 150,
+  radius: 70,
 };
 
 // Add event listener
@@ -16,21 +16,19 @@ const mouse = {
 canvas.addEventListener("mousemove", function (event) {
   mouse.x = event.x;
   mouse.y = event.y;
-  //   console.log(mouse.x, mouse.y);
 });
 
 // Adding Font using CTX
 ctx.fillStyle = "black";
-ctx.font = "bold 50px Arial";
-ctx.wrap;
-ctx.fillText("Welcome", 10, 40);
+ctx.font = "bold 15px Arial";
+ctx.fillText("Welcome", 50, 35);
 
 // Get image data for pixel manipulation
 
-//ctx.strokeRect(0, 0, 240, 50); //To check the outline of image being selected
-const data = ctx.getImageData(0, 0, 240, 50);
+ctx.strokeRect(10, 0, 500, 120); //To check the outline of image being selected
+const textCoordinates = ctx.getImageData(10, 0, 500, 120);
 
-//Creates Class of particles
+//Creates class of particles
 class Particles {
   constructor(x, y) {
     this.x = x;
@@ -38,6 +36,7 @@ class Particles {
     this.size = 3;
     this.baseX = this.x;
     this.baseY = this.y;
+    this.density = Math.random() * 40 + 5;
   }
 
   draw() {
@@ -49,13 +48,29 @@ class Particles {
   }
 
   update() {
-    let dx = mouse.x - this.x;
-    let dy = mouse.y - this.y;
-    let distance = Math.sqrt(dx * dx + dy * dy);
-    if (distance < 100) {
-      this.size = 15;
+    let dx = mouse.x - this.x; //Distance between mouse pointer and the particle in x direction
+    let dy = mouse.y - this.y; //Distance between mouse pointer and the particle in y direction
+    let distance = Math.sqrt(dx * dx + dy * dy); //Direct distance between mouse pointer and the particle using hypotenuse
+    let forceDirectionX = dx / distance;
+    let forceDirectionY = dy / distance;
+    let maxDistance = mouse.radius;
+    let force = (maxDistance - distance) / maxDistance;
+    let directionX = forceDirectionX * force * this.density;
+    let directionY = forceDirectionY * force * this.density;
+
+    if (distance < mouse.radius) {
+      this.x -= directionX / 10;
+      this.y -= directionY / 10;
     } else {
-      this.size = 3;
+      if (this.x !== this.baseX) {
+        let dx = this.x - this.baseX;
+        this.x -= dx / 5;
+      }
+
+      if (this.y !== this.baseY) {
+        let dy = this.y - this.baseY;
+        this.y -= dy / 5;
+      }
     }
   }
 }
@@ -64,15 +79,21 @@ let particleArray = []; //All new Particles objects will be stored in an array
 //Initialize the Particles class
 function init() {
   particleArray = [];
-  for (let i = 0; i < 1000; i++) {
-    let x = Math.random() * canvas.width;
-    let y = Math.random() * canvas.height;
-    particleArray.push(new Particles(x, y));
+  for (let y = 0, y2 = textCoordinates.height; y < y2; y++) {
+    for (let x = 0, x2 = textCoordinates.width; x < x2; x++) {
+      if (
+        textCoordinates.data[y * 4 * textCoordinates.width + x * 4 + 3] > 128
+      ) {
+        let positionX = x;
+        let positionY = y;
+        particleArray.push(new Particles(positionX * 10, positionY * 10));
+      }
+    }
   }
 }
 
 init();
-console.log(particleArray);
+// console.log(particleArray);
 
 function animate() {
   ctx.clearRect(0, 0, canvas.width, canvas.height);
@@ -80,7 +101,30 @@ function animate() {
     particleArray[i].draw();
     particleArray[i].update();
   }
+  connect();
   requestAnimationFrame(animate);
 }
 
 animate();
+
+//Connecting line between particles
+function connect() {
+  let opacityValue = 1;
+  for (let outer = 0; outer < particleArray.length; outer++) {
+    for (let inner = outer; inner < particleArray.length; inner++) {
+      let dx = particleArray[outer].x - particleArray[inner].x;
+      let dy = particleArray[outer].y - particleArray[inner].y;
+      let distance = Math.sqrt(dx * dx + dy * dy);
+
+      if (distance < 30) {
+        opacityValue = 1 - distance / 30;
+        ctx.strokeStyle = "rgba(0,0,0, " + opacityValue + ")";
+        ctx.lineWidth = 0.5;
+        ctx.beginPath();
+        ctx.moveTo(particleArray[outer].x, particleArray[outer].y);
+        ctx.lineTo(particleArray[inner].x, particleArray[inner].y);
+        ctx.stroke();
+      }
+    }
+  }
+}
